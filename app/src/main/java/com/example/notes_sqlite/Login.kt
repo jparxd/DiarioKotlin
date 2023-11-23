48299c8d71fb25d310532700c8542cc96196998e
jparxd <jparxd@gmail.com>
2023-11-23 20:32:16 -0300

    Aplicativo de Diário

---
+++ app/src/main/java/com/example/notes_sqlite/Login.kt
@@ -0,0 +1,19 @@
+package com.example.notes_sqlite
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+//import com.example.notes_sqlite.databinding.ActivityLoginBinding
+import com.example.notes_sqlite.databinding.ActivityMainBinding
+
+class Login : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+    }
+}
